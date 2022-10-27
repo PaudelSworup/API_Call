26192126cf138d03c617a68093186c478ca17325
PaudelSworup <sworupkc9@gmail.com>
2022-10-27 14:41:40 +0545

    api call

---
+++ index.js
@@ -0,0 +1,21 @@
+const div = document.getElementById("main-div");
+console.log(div);
+
+async function getData() {
+  const response = await fetch("http://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+  console.log(data);
+
+  data.map((cur) => {
+    div.innerHTML =
+      div.innerHTML +
+      `<div class="sub-main">
+      <p>userId : ${cur.userId}</p>
+      <p>id : ${cur.id} </p>
+        <h3>${cur.title}</h3>
+        <p>${cur.body}</p>
+    </div>`;
+  });
+}
+
+getData();
